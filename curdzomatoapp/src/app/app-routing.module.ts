9d592da12f581885d085e0c8adf03eb5a349dfcc
nitinmirge <nitinmirge123@gmail.com>
2023-12-25 14:16:59 +0530

    routing updated

---
+++ curdzomatoapp/src/app/app-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FoodsComponent } from './components/foods/foods.component';
+import { RestarantfooditemsComponent } from './components/restarantfooditems/restarantfooditems.component';
+import { RestaurantservingfoodComponent } from './components/restaurantservingfood/restaurantservingfood.component';
+
+
+const routes: Routes = [ 
+  {
+    path: 'foods',
+    component: FoodsComponent
+  },
+  {
+    path: 'restaurant-foods/:id',
+    component: RestarantfooditemsComponent
+  }, 
+  {
+    path: 'restaurant-food-item/:restaurantId/:categoryId',
+    component: RestaurantservingfoodComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
